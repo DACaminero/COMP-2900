84ad24c8a8f2837ef3d46e4af3d3d3c45fb63364
DACaminero <dcaminero3180@arecibointer.edu>
2024-02-29 13:24:17 -0400

    update

--- Modulo01/Lab05/app.py
+++ Modulo01/Lab05/app.py
@@ -7,3 +7,9 @@ def saludo (mensaje):
     print(f"!Hola, { mensaje } ! ")
 
 saludo("Mundo")
+
+def suma (a,b):
+    return (a+b) #retorna la suma de a + b
+
+resultado = suma(5,4) #llamar ala funcion  y recibir el resultado
+print(resultado) #imprimir el resultado
